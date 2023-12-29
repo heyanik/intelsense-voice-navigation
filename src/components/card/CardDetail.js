4e3d02afb03dd8ca332e42def5a7d099880b077c
heyanik <anikdaspartha58@gmail.com>
2023-12-29 14:40:38 +0600

    add some changes

--- src/components/card/CardDetail.js
+++ src/components/card/CardDetail.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import "../../styles/Searchbar.css"
+import React from "react";
+import { useParams } from "react-router-dom";
+import "../../styles/Searchbar.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const CardDetail = ({ person }) => {
   const { id } = useParams();
-
 
   if (!person) {
     return <div>Loading...</div>;
   }
 
-
   const card = person.find((card) => card.id === id);
 
   if (!card) {
-    return <div>
-
+    return (
+      <div>
         <h1>Card not found</h1>
-    </div>;
+      </div>
+    );
   }
 
   return (
     <>
-    <div className='tc navbar'>
-      <h2>{card.name}</h2>
-    </div>
-    <div>
-      <p></p>
-    </div>
+      <div className="tc navbar">
+        <a href="javascript:window.history.back();">
+          <IoMdArrowRoundBack className="icon" />
+        </a>
+        <h2>{card.name}</h2>
+      </div>
+      <div className="page-img">
+        <img src={card.imgPath} alt="imagePath" />
+      </div>
     </>
   );
 };
